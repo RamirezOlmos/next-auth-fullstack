580222f02679ad867853ef0b9640c65bc28964cd
RamirezOlmos <nyy_715074@yahoo.com>
2023-06-25 21:20:58 -0600

    added(src/app/UserEdit/page.tsx) added the last part to verify that the id is added to the form to send the change to the character in the database.

--- src/app/UserEdit/page.tsx
+++ src/app/UserEdit/page.tsx
@@ -9,48 +9,19 @@ interface Character {
   id: string;
   name: string;
   status: string;
-  [key: string]: string; // Index signature allowing string indexing
 }
 
-
 const UserEdit = () => {
-  const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
   const [inputs, setInputs] = useState<Character>({
     id: '',
     name: '',
     status: '',
   });
 
-  const [errors, setErrors] = useState<Character>({
-    id: '',
-    name: '',
-    status: '',
-  });
-
-
-  const validate = (inputs: Character) => {
-    const errors: Character = {
-      id: '',
-      name: '',
-      status: ''
-    };
-    if (!inputs.id)
-      errors.id = 'Id is require';
-
-    return errors;
-  };
-
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setInputs({ ...inputs, [name]: value });
-
-    const updatedErrors = validate({ ...inputs, [name]: value });
-    setErrors(updatedErrors);
-
-    // Enable the button when there are no errors
-    const hasErrors = Object.keys(updatedErrors).some((key) => updatedErrors[key] !== '');
-    setButtonDisabled(hasErrors);
 
   };
 
@@ -71,7 +42,7 @@ const UserEdit = () => {
     event.target.reset();
     try {
       const response = await fetch(
-        `http://localhost:3000/api/deletecharacters?id=${inputs.id}`, {
+        `http://localhost:3000/api/editcharacters?id=${inputs.id}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
@@ -80,27 +51,19 @@ const UserEdit = () => {
       });
 
       if (!response.ok) {
-        throw new Error('Failed to create the character');
+        throw new Error('Failed to change the character');
       }
 
       // Check if all the required properties exist
       if (response.ok) {
-        alert('Character created successfully');
-      }
-
-      if (!Object.keys(errors).length) {
         setInputs({
           id: '',
           name: '',
           status: '',
         });
-
-        setErrors({
-          id: '',
-          name: '',
-          status: '',
-        });
+        alert('Character change successfully');
       }
+
     } catch (error) {
       console.error(error);
     }
@@ -120,39 +83,38 @@ const UserEdit = () => {
           onSubmit={handleSubmit}>
           <label htmlFor="id">Id: </label>
           <input
-            className={` ${errors.id ? 'warning' : ''} w-450 bg-lightsteelblue`}
+            className={` ${inputs.id ? 'warning' : ''} w-450 bg-lightsteelblue`}
             type="text"
             name="id"
             placeholder="Character id..."
             value={inputs.id}
             onChange={handleInputChange}
           ></input>
-          <div className='text-red-500'> {errors.id}</div>
-          <label htmlFor="status">Status: </label>
+          <div className='text-red-500'>
+            {!inputs.id ? 'Id is require to submit input' : ''}
+          </div>
           <label htmlFor="name">Name: </label>
           <input
-            className={` ${errors.name ? 'warning' : ''} w-450 bg-lightsteelblue`}
+            className={`w-450 bg-lightsteelblue`}
             type="text"
             name="name"
             placeholder="Character name..."
             value={inputs.name}
             onChange={handleInputChange}
           ></input>
-          <div className='text-red-500'> {errors.name}</div>
           <label htmlFor="status">Status: </label>
           <input
-            className={` ${errors.name ? 'warning' : ''} w-450 bg-lightsteelblue`}
+            className={`w-450 bg-lightsteelblue`}
             name="status"
             placeholder="Character status..."
             value={inputs.status}
             onChange={handleInputChange}
           ></input>
-          <div className='text-red-500'> {errors.status}</div>
           <div className="flex justify-end mt-4">
             <button type="submit"
-              disabled={buttonDisabled}
+              disabled={!inputs.id}
               className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 
-                        px-4 rounded ${buttonDisabled ? 'opacity-50 cursor-not-allowed' : ''
+                        px-4 rounded ${!inputs.id ? 'opacity-50 cursor-not-allowed' : ''
                 } mx-auto mt-4`}
             >
               Change
