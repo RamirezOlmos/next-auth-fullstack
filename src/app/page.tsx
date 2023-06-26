0f6891c594968f4dfcdbf41be6d86b22211a7f4f
RamirezOlmos <nyy_715074@yahoo.com>
2023-06-25 20:43:38 -0600

    edited(src/app/page.tsx) edited the main page app to be able to show characters in the database and add the delete component to be able to delete characters with the id of character.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 
 
 import ShowCharacters from '../components/ShowCharacters';
+import DeleteCharacter from '../components/DeleteCharacter';
 
 export default function Home() {
   return (
     <div className="flex flex-wrap gap-5 justify-center items-center">
       <div className='flex-center'>
-        <div>
+        <div className='mt-4 mb-4'>
           Get characters from data base
         </div>
         <ShowCharacters />
 
       </div>
-
+      <div className="flex items-center">
+        <div className="ml-44"> {/* Add any desired margin here */}
+          <DeleteCharacter />
+        </div>
+      </div>
     </div>
   )
 }
